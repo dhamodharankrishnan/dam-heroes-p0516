82d604b70958bc58e7c58a3dd2257b8061de7b18
Dhamodharan Krishnan <dhamodharan.krishnan@walgreens.com>
2020-05-24 11:19:30 -0400

    Get Data from Server

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 
+import { MessageService } from '../message.service';
+import { HeroService } from '../hero.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -10,15 +13,31 @@ import { HEROES } from '../mock-heroes';
 
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
-  selectedHero: Hero;
+//  heroes = HEROES;
+heroes: Hero[];
+selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService, private messageService: MessageService) {
+    this.printSomething('In constructor');
+  }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
+    this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
     this.selectedHero = hero;
   }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(incomingHeroes => this.heroes = incomingHeroes);
+    this.printSomething('getHeroes');
+  }
+
+  printSomething(logValue: string) {
+    console.log(logValue);
+  }
+
+
 }
